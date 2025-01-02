ac6027621f147bc21970ebbd83d96767c393e26f
shiveshdewangan <shivesh.dewangan@gmail.com>
2025-01-02 11:37:54 +1100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://fullstack-backend-s31w.onrender.com/',
         secure: false,
         changeOrigin: true,
       },
