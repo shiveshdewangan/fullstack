f98035720f5be8ce7435222e899c74e61899a331
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2025-01-02 20:23:43 +1100

    hello world

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     open: true,
     proxy: {
-      '/api': {
-        target: 'https://fullstack1-mse6.onrender.com/',
+      "/api": {
+        target: "http://localhost:3001",
         secure: false,
         changeOrigin: true,
       },
